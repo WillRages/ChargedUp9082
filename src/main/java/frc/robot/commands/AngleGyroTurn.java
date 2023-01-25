20c979298b666165bc62f6d8f7e14e51893cd1ec
FTC-6093 <89921520+Mahoroah@users.noreply.github.com>
2023-01-24 20:01:10 -0600

    UPDATED GyroTurn

--- src/main/java/frc/robot/commands/AngleGyroTurn.java
+++ src/main/java/frc/robot/commands/AngleGyroTurn.java
@@ -5,24 +5,51 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
 
 public class AngleGyroTurn extends CommandBase {
   /** Creates a new AngleGyroTurn. */
-  public AngleGyroTurn() {
+  private double angle;
+  private Drivetrain drivetrain;
+  private double speed;
+  private double target_head;
+
+  public AngleGyroTurn(Drivetrain drivetrain, double angle, double speed) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.speed = speed;
+    this.angle = angle;
+    this.drivetrain = drivetrain;
+    addRequirements(drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    angle = target_head;
+    if (target_head < 0) {
+      speed = -speed;
+      while (drivetrain.getHeading() > target_head) {
+        drivetrain.arcadeDrive(0, speed);
+      }
+    }
+    while (drivetrain.getHeading() < target_head) {
+      drivetrain.arcadeDrive(0, speed);
+    }
+    drivetrain.getHeading();
+    drivetrain.arcadeDrive(0, speed);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drivetrain.arcadeDrive(0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
