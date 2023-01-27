38782851b6a19bb8ce99a00f03e579e8fc198834
Will Rages <rageswill@pleasval.org>
2023-01-26 22:13:22 -0600

    Convert spaces to tabs

--- src/main/java/frc/robot/commands/TurnToAngleGyro.java
+++ src/main/java/frc/robot/commands/TurnToAngleGyro.java
@@ -8,55 +8,55 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
 public class TurnToAngleGyro extends CommandBase {
-  /** Creates a new AngleGyroTurn. */
-  private Drivetrain drivetrain;
-  private double speed;
-  private double target_head;
+	/** Creates a new AngleGyroTurn. */
+	private Drivetrain drivetrain;
+	private double speed;
+	private double target_head;
 
-  public TurnToAngleGyro(Drivetrain drivetrain, double angle, double speed) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.speed = speed;
-    this.target_head = angle;
-    this.drivetrain = drivetrain;
-    addRequirements(drivetrain);
-  }
+	public TurnToAngleGyro(Drivetrain drivetrain, double angle, double speed) {
+		// Use addRequirements() here to declare subsystem dependencies.
+		this.speed = speed;
+		this.target_head = angle;
+		this.drivetrain = drivetrain;
+		addRequirements(drivetrain);
+	}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    drivetrain.getHeading();
-  }
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		drivetrain.getHeading();
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (target_head < 0) {
-      speed = -speed;
-      while (drivetrain.getHeading() > target_head) {
-        drivetrain.arcadeDrive(0, speed);
-      }
-    }
-    while (drivetrain.getHeading() < target_head) {
-      drivetrain.arcadeDrive(0, speed);
-    }
-    drivetrain.getHeading();
-    drivetrain.arcadeDrive(0, speed);
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		if (target_head < 0) {
+			speed = -speed;
+			while (drivetrain.getHeading() > target_head) {
+				drivetrain.arcadeDrive(0, speed);
+			}
+		}
+		while (drivetrain.getHeading() < target_head) {
+			drivetrain.arcadeDrive(0, speed);
+		}
+		drivetrain.getHeading();
+		drivetrain.arcadeDrive(0, speed);
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    drivetrain.arcadeDrive(0, 0);
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		drivetrain.arcadeDrive(0, 0);
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    // check if we are within 10 degrees of the target
-    if (Math.abs(drivetrain.getHeading() - target_head) < 10) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		// check if we are within 10 degrees of the target
+		if (Math.abs(drivetrain.getHeading() - target_head) < 10) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- src/main/java/frc/robot/commands/TurnToAngleTime.java
+++ src/main/java/frc/robot/commands/TurnToAngleTime.java
@@ -10,45 +10,45 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
 public class TurnToAngleTime extends CommandBase {
-  /** Creates a new TurnToAngleTime. */
-  private Drivetrain drivetrain;
-  private final Timer autoTimer;
-  private final double time;
-  private final double speed;
+	/** Creates a new TurnToAngleTime. */
+	private Drivetrain drivetrain;
+	private final Timer autoTimer;
+	private final double time;
+	private final double speed;
 
-  public TurnToAngleTime(Drivetrain drivetrain, double time, double speed) {
-    this.time = time;
-    this.speed = speed;
-    this.drivetrain = drivetrain;
-    this.autoTimer = new Timer();
-    addRequirements(drivetrain);
-  }
+	public TurnToAngleTime(Drivetrain drivetrain, double time, double speed) {
+		this.time = time;
+		this.speed = speed;
+		this.drivetrain = drivetrain;
+		this.autoTimer = new Timer();
+		addRequirements(drivetrain);
+	}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    autoTimer.reset();
-    autoTimer.start();
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		autoTimer.reset();
+		autoTimer.start();
 
-    drivetrain.arcadeDrive(0, speed);
-  }
+		drivetrain.arcadeDrive(0, speed);
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    drivetrain.arcadeDrive(0, speed);
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		drivetrain.arcadeDrive(0, speed);
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    drivetrain.arcadeDrive(0, 0);
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		drivetrain.arcadeDrive(0, 0);
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    SmartDashboard.putNumber("Auto Running", ((double) ((int) (autoTimer.get() * 10))) / 10);
-    return autoTimer.get() >= time;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		SmartDashboard.putNumber("Auto Running", ((double) ((int) (autoTimer.get() * 10))) / 10);
+		return autoTimer.get() >= time;
+	}
 }
